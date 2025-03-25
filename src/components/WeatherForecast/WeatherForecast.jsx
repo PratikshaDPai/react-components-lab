b57db5edde637dbfe35aca78861ff5bca2277682
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-25 14:45:52 -0700

    Add WeatherIcon

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,10 +1,12 @@
 import "./WeatherForecast.css";
+import WeatherIcon from "./WeatherIcon.jsx";
 
 const weatherForecast = (props) => {
   const { weather } = props;
   return (
     <div className="weather">
       <h2>{weather.day}</h2>
+      <WeatherIcon image={weather.img} imagealt={weather.imgAlt} />
       <img src="" alt="" />
       <p>
         <span>conditions: </span>
---
+++ src/components/WeatherForecast/WeatherIcon.jsx
@@ -0,0 +1,7 @@
+import "./WeatherForecast.css";
+
+const WeatherIcon = ({ image }) => {
+  return <img src={image}></img>;
+};
+
+export default WeatherIcon;

ccfbfbd149d78ecdd028533e2f74bb0f2de8646e
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-25 14:51:53 -0700

    Add WeatherDay, use spread operator to split weather

---
+++ src/components/WeatherForecast/WeatherDay.jsx
@@ -0,0 +1,18 @@
+const WeatherDay = ({ day, conditions, time }) => {
+  return (
+    <div>
+      <h2>{day}</h2>
+
+      <p>
+        <span>conditions: </span>
+        {conditions}
+      </p>
+      <p>
+        <span>time: </span>
+        {time}
+      </p>
+    </div>
+  );
+};
+
+export default WeatherDay;
--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,21 +1,13 @@
 import "./WeatherForecast.css";
 import WeatherIcon from "./WeatherIcon.jsx";
+import WeatherDay from "./WeatherDay.jsx";
 
 const weatherForecast = (props) => {
   const { weather } = props;
   return (
     <div className="weather">
-      <h2>{weather.day}</h2>
       <WeatherIcon image={weather.img} imagealt={weather.imgAlt} />
-      <img src="" alt="" />
-      <p>
-        <span>conditions: </span>
-        {weather.conditions}
-      </p>
-      <p>
-        <span>time: </span>
-        {weather.time}
-      </p>
+      <WeatherDay {...weather} />
     </div>
   );
 };
